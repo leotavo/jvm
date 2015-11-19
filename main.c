0eba841f17a717490551f6bdfeac6eeb96791e09
Leonardo Trindade <leotavo@gmail.com>
2015-11-19 15:49:27 +0000

    main.c

--- main.c
+++ main.c
@@ -23,22 +23,11 @@ int	main(int, char **);	// função principal do programa
 // função main()
 // - Trata os dados de entrada.
 int	main(int argc, char* argv[]){
-	FILE *	input;
-	
 	switch(argc){
 		case	1:
 			puts("Uso: ./jvm arquivo.class [args...]\n");
 			return	0;
 		default:
-			input = fopen(argv[1],"r");
-			if(!input){
-				printf("Erro ao abrir o arquivo %s.\n", argv[1]);
-				return 0;
-			}
-			else{
-				jvmStart(input, argc-2, argv+2);
-				fclose(input);
-			}
-	}
+			jvmStart(argv[1], argc-2, argv+2);
 }// fim da função main()
 /*==========================================*/
